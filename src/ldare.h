7cd28010c9b91cefd84e9c863e8fc22769428130
= <marciovmf@gmail.com>
2017-07-07 00:53:43 -0300

    Moved global macros and definitions to a separated header file

---
+++ src/ldare.h
@@ -0,0 +1,33 @@
+#ifndef _LDARE_H_
+#define _LDARE_H_
+
+#ifdef DEBUG
+#	include <iostream>
+#	ifdef UNICODE
+#		define OUTSTREAM std::wcout
+#		define ERRSTREAM std::wcerr
+#	else
+#		define OUTSTREAM std::cout
+#		define ERRSTREAM std::cerr
+#	endif // UNICODE
+#	define LogMsg(prefix, msg, stdstream) stdstream << prefix << " " << msg << "\n" << __FILE__ << ":" << __LINE__ <<  std::endl
+#	define LogInfo(msg) LogMsg("[INFO]", msg, OUTSTREAM)
+#	define LogWarning(msg) LogMsg("[WARNING]", msg, OUTSTREAM)
+#	define LogError(msg) LogMsg("[ERROR]", msg, OUTSTREAM)
+#else
+#	define LogMsg(prefix, msg, stdstream)
+#	define LogError(msg) 
+#	define LogWarning(msg) 
+#	define LogError(msg) 
+#endif // DEBUG
+
+#include <stdint.h>
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+#endif //_LDARE_H_
--- src/win32/win32_ldare.cpp
+++ src/win32/win32_ldare.cpp
@@ -1,31 +1,9 @@
+#include "../ldare.h"
 #include <windows.h>
 #include <tchar.h>
 
 #include "../GL/glcorearb.h"
 #include "../GL/wglext.h"
-
-#ifdef DEBUG
-#include <iostream>
-
-#ifdef UNICODE
-#define OUTSTREAM std::wcout
-#define ERRSTREAM std::wcerr
-#else
-#define OUTSTREAM std::cout
-#define ERRSTREAM std::cerr
-#endif // UNICODE
-
-#define LogMsg(prefix, msg, stdstream) stdstream << prefix << " " << msg << "\n" << __FILE__ << ":" << __LINE__ <<  std::endl
-#define LogInfo(msg) LogMsg("[INFO]", msg, OUTSTREAM)
-#define LogWarning(msg) LogMsg("[WARNING]", msg, OUTSTREAM)
-#define LogError(msg) LogMsg("[ERROR]", msg, OUTSTREAM)
-#else
-#define LogMsg(prefix, msg, stdstream)
-#define LogError(msg) 
-#define LogWarning(msg) 
-#define LogError(msg) 
-
-#endif // DEBUG
 
 #define GAME_WINDOW_CLASS "LDARE_WINDOW_CLASS"
 
