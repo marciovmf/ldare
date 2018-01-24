110703d712b9af02c8396b4ecae1600c636de1dd
= <marciovmf@gmail.com>
2018-01-24 01:17:57 -0200

    Begin implementing rendering backend functions for GPU buffers

---
+++ src/ldare_renderer_buffer.h
@@ -0,0 +1,77 @@
+#ifndef __LDARE_RENDERER_BUFFER__	
+#define __LDARE_RENDERER_BUFFER__	
+
+	//
+	// The renderer namespace is for the rendere backend only. 
+	// It will not be exposed to the game layer.
+	//
+	namespace renderer
+	{	
+		//
+		// Describes attributes of a GPU buffer layout
+		//
+		struct BufferLayout
+		{
+			enum Type{ INT8, UINT8, INT16, UINT16, FLOAT32 };
+			enum Size{ X1=1, X2, X3, X4};
+
+			uint32 index;
+			Type type;
+			Size size;
+			uint32 start;
+			uint32 offset;
+		};
+
+		//
+		// A GPU buffer that can be filled with data for rendering.
+		//
+		struct Buffer
+		{
+			enum Type{ VERTEX, VERTEX_DYNAMIC, INDEX, UNIFORM };
+			uint32 id;
+			Type type;
+		};
+
+		//
+		// Creates a buffer with the given size and layout
+		// return a GPU buffer identifier
+		//
+		renderer::Buffer createBuffer(renderer::Buffer::Type, size_t size, const renderer::BufferLayout* layout, uint32 layoutCount);
+
+		//
+		// Copies data to a GPU buffer, partially overwitting its content
+		// return amount of data coppied to buffer
+		//
+		size_t setBufferData(const renderer::Buffer* buffer, void* data, size_t dataSize, void* startOffset);
+
+		//
+		// Copies data to a GPU buffer, completely overwitting its content
+		// return amount of data coppied to buffer
+		//
+		size_t setBufferData(const renderer::Buffer* buffer, void* data, size_t dataSize);
+
+		//
+		// Binds the buffer to the GPU so it can be used
+		//
+ 		void bindBuffer(const renderer::Buffer* buffer);
+	
+		//
+		// Unbinds the buffer from the GPU
+		//
+ 		void unbindBuffer(renderer::Buffer* buffer);
+
+		//
+		// Deletes a GPU buffer and release its GPU resources 
+		//
+		void deleteBuffer(renderer::Buffer* buffer);
+
+	}
+
+//	using namespace ldare::renderer;
+//	BufferLayout layout[] = {
+//		{0, BufferLayout::Type::FLOAT32, BufferLayout::Size::X4, 0, 0 },
+//		{1, BufferLayout::Type::FLOAT32, BufferLayout::Size::X4, 0, 0 },
+//		{2, BufferLayout::Type::FLOAT32, BufferLayout::Size::X4, 0, 0 }};
+
+
+#endif // __LDARE_RENDERER_BUFFER__	
