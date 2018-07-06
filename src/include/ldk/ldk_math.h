365fc7caed57246da6e68475d26ddbbbf6b75e01
Márcio Freitas <marcio.freitas@aquiris.com.br>
2018-07-06 15:18:21 -0300

    Add math functions to the engine dll

--- src/include/ldk/ldk_math.h
+++ src/include/ldk/ldk_math.h
@@ -19,335 +19,110 @@ namespace ldk
 		float x, y, w, h;
 	};
 
-	static const float _zero[] = {0,0,0,0};
-	static const float _one[] = {1,1,1,1};
-
 	//---------------------------------------------------------------------------
 	// Vector2 
 	//---------------------------------------------------------------------------
 	struct Vec2
 	{
 		float x, y;
-		float operator[](int32 n)
-		{ LDK_ASSERT(n >= 0 && n < 2,"vector index should be between positive and within vector size");	
-			return (&x)[n]; }
+		float operator[](int32 n);
 
-		bool operator==(const Vec2& other)
-		{
-			return (int)	this->x == (int)	other.x && (int)	this->y == (int)	other.y ;
-		}
+		bool operator==(const Vec2& other);
 
-		Vec2 operator+(const Vec2& other)
-		{
-			Vec2 result;
-			result.x = this->x + other.x;
-			result.y = this->y + other.y;
-			return result;
-		}
+		Vec2 operator+(const Vec2& other);
 
-		Vec2 operator-(const Vec2& other)
-		{
-			Vec2 result;
-			result.x = this->x - other.x;
-			result.y = this->y - other.y;
-			return result;
-		}
+		Vec2 operator-(const Vec2& other);
 
-		Vec2 operator*(const Vec2& other)
-		{
+		Vec2 operator*(const Vec2& other);
 
-			Vec2 result;
-			result.x = this->x * other.x;
-			result.y = this->y * other.y;
-			return result;
-		}
+		Vec2 operator/(const Vec2& other);
 
-		Vec2 operator/(const Vec2& other)
-		{
-			Vec2 result;
-			result.x = this->x / other.x;
-			result.y = this->y / other.y;
-			return result;
-		}
+		Vec2& operator+=(const Vec2& other);
 
-		Vec2& operator+=(const Vec2& other)
-		{
-			this->x += other.x;
-			this->y += other.y;
-			return *this;
-		}
+		Vec2& operator-=(const Vec2& other);
 
-		Vec2& operator-=(const Vec2& other)
-		{
-			this->x -= other.x;
-			this->y -= other.y;
-			return *this;
-		}
+		Vec2& operator*=(const Vec2& other);
 
-		Vec2& operator*=(const Vec2& other)
-		{
-			this->x *= other.x;
-			this->y *= other.y;
-			return *this;
-		}
+		Vec2& operator/=(const Vec2& other);
 
-		Vec2& operator/=(const Vec2& other)
-		{
-			this->x /= other.x;
-			this->y /= other.y;
-			return *this;
-		}
+		float magnitude();
 
-		float magnitude()
-		{
-			return sqrt(x * x + y * y);
-		}
+		static const Vec2& one();
 
-		static const Vec2& one()
-		{
-			return (const Vec2&)_one;
-		}
-
-		static const Vec2& zero()
-		{
-			return (const Vec2& )_zero;
-		}
+		static const Vec2& zero();
 	};
 
 	//---------------------------------------------------------------------------
 	// Vector3
 	//---------------------------------------------------------------------------
-	struct Vec3
+	struct LDK_API_CLASS Vec3
 	{
 		float x, y, z;
 
-		float operator[](int32 n)
-		{ 
-			LDK_ASSERT(n >= 0 && n < 3, "vector index should be between positive and within vector size");
-			return (&x)[n]; 
-		}
+		float operator[](int32 n);
 
-		bool operator==(const Vec3& other)
-		{
-			return  (int)	this->x == (int)	other.x && 
-				(int)	this->y == (int)	other.y && 
-				(int)	this->z == (int)	other.z;
-		}
+		bool operator==(const Vec3& other);
 
-		Vec3 operator+(const Vec3& other)
-		{
-			Vec3 result;
-			result.x = this->x + other.x;
-			result.y = this->y + other.y;
-			result.z = this->z + other.z;
-			return result;
-		}
+		Vec3 operator+(const Vec3& other);
 
-		Vec3 operator-(const Vec3& other)
-		{
-			Vec3 result;
-			result.x = this->x - other.x;
-			result.y = this->y - other.y;
-			result.z = this->z - other.z;
-			return result;
-		}
+		Vec3 operator-(const Vec3& other);
 
-		Vec3 operator*(float scalar)
-		{
+		Vec3 operator*(float scalar);
 
-			Vec3 result;
-			result.x = this->x * scalar;
-			result.y = this->y * scalar;
-			result.z = this->z * scalar;
-			return result;
-		}
+		Vec3 operator*(const Vec3& other);
 
-		Vec3 operator*(const Vec3& other)
-		{
+		Vec3 operator/(const Vec3& other);
 
-			Vec3 result;
-			result.x = this->x * other.x;
-			result.y = this->y * other.y;
-			result.z = this->z * other.z;
-			return result;
-		}
+		Vec3& operator+=(const Vec3& other);
 
-		Vec3 operator/(const Vec3& other)
-		{
-			Vec3 result;
-			result.x = this->x / other.x;
-			result.y = this->y / other.y;
-			result.z = this->z / other.z;
-			return result;
-		}
+		Vec3& operator-=(const Vec3& other);
 
-		Vec3& operator+=(const Vec3& other)
-		{
-			this->x += other.x;
-			this->y += other.y;
-			this->z += other.z;
-			return *this;
-		}
+		Vec3& operator*=(const Vec3& other);
 
-		Vec3& operator-=(const Vec3& other)
-		{
-			this->x -= other.x;
-			this->y -= other.y;
-			this->z -= other.z;
-			return *this;
-		}
+		Vec3& operator*=(float scalar);
 
-		Vec3& operator*=(const Vec3& other)
-		{
-			this->x *= other.x;
-			this->y *= other.y;
-			this->z *= other.z;
-			return *this;
-		}
+		Vec3& operator/=(const Vec3& other);
 
-		Vec3& operator*=(float scalar)
-		{
-			this->x *= scalar;
-			this->y *= scalar;
-			this->z *= scalar;
-			return *this;
-		}
+		float magnitude();
 
-		Vec3& operator/=(const Vec3& other)
-		{
-			this->x /= other.x;
-			this->y /= other.y;
-			this->z /= other.z;
-			return *this;
-		}
+		static const Vec3& one();
 
-		float magnitude()
-		{
-			return sqrt(x * x + y * y + z * z);
-		}
-
-		static const Vec3& one()
-		{
-			return (const Vec3&)_one;
-		}
-
-		static const Vec3& zero()
-		{
-			return (const Vec3& )_zero;
-		}
+		static const Vec3& zero();
 
 	};
 
 	//---------------------------------------------------------------------------
 	// Vector4
 	//---------------------------------------------------------------------------
-	struct Vec4
+	struct LDK_API_CLASS Vec4
 	{
 		float x, y, z, w;
 
-		float operator[](int32 n)
-		{ 
-			LDK_ASSERT(n >= 0 && n < 4, "vector index should be between positive and within vector size");
-			return (&x)[n]; 
-		}
+		float operator[](int32 n);
 
-		bool operator==(const Vec4& other)
-		{
-			return (int)	this->x == (int)	other.x && (int)	this->y == (int)	other.y &&
-				(int)	this->z == (int)	other.z && (int)	this->w == (int)	other.w;
-		}
+		bool operator==(const Vec4& other);
 
-		Vec4 operator+(const Vec4& other)
-		{
-			Vec4 result;
-			result.x = this->x + other.x;
-			result.y = this->y + other.y;
-			result.z = this->z + other.z;
-			result.w = this->w + other.w;
-			return result;
-		}
+		Vec4 operator+(const Vec4& other);
 
-		Vec4 operator-(const Vec4& other)
-		{
-			Vec4 result;
-			result.x = this->x - other.x;
-			result.y = this->y - other.y;
-			result.z = this->z - other.z;
-			result.w = this->w - other.w;
-			return result;
-		}
+		Vec4 operator-(const Vec4& other);
 
-		Vec4 operator*(const Vec4& other)
-		{
+		Vec4 operator*(const Vec4& other);
 
-			Vec4 result;
-			result.x = this->x * other.x;
-			result.y = this->y * other.y;
-			result.z = this->z * other.z;
-			result.w = this->w * other.w;
-			return result;
-		}
+		Vec4 operator/(const Vec4& other);
 
-		Vec4 operator/(const Vec4& other)
-		{
-			Vec4 result;
-			result.x = this->x / other.x;
-			result.y = this->y / other.y;
-			result.z = this->z / other.z;
-			result.w = this->w / other.w;
-			return result;
-		}
+		Vec4& operator+=(const Vec4& other);
 
-		Vec4& operator+=(const Vec4& other)
-		{
-			this->x += other.x;
-			this->y += other.y;
-			this->z += other.z;
-			this->w += other.w;
-			return *this;
-		}
+		Vec4& operator-=(const Vec4& other);
 
-		Vec4& operator-=(const Vec4& other)
-		{
-			this->x -= other.x;
-			this->y -= other.y;
-			this->z -= other.z;
-			this->w -= other.w;
-			return *this;
-		}
+		Vec4& operator*=(const Vec4& other);
 
-		Vec4& operator*=(const Vec4& other)
-		{
-			this->x *= other.x;
-			this->y *= other.y;
-			this->z *= other.z;
-			this->w *= other.w;
-			return *this;
-		}
+		Vec4& operator/=(const Vec4& other);
 
-		Vec4& operator/=(const Vec4& other)
-		{
-			this->x /= other.x;
-			this->y /= other.y;
-			this->z /= other.z;
-			this->w /= other.w;
-			return *this;
-		}
+		float magnitude();
 
-		float magnitude()
-		{
-			return sqrt(x * x + y * y + z * z + w * w);
-		}
+		static const Vec4& one();
 
-		static const Vec4& one()
-		{
-			return (const Vec4&)_one;
-		}
-
-		static const Vec4& zero()
-		{
-			return (const Vec4& )_zero;
-		}
+		static const Vec4& zero();
 	};
 
 	//---------------------------------------------------------------------------
@@ -363,174 +138,49 @@ namespace ldk
 	// Column 2 is back
 	// Column 3 is position
 	//---------------------------------------------------------------------------
-	struct Mat4
+	struct LDK_API_CLASS Mat4
 	{
 		union{
 			float element[16];
 			Vec4 column[4];
 		};
 
-		Mat4()
-		{
-			column[0] ={};
-			column[1] ={};
-			column[2] ={};
-			column[3] ={};
-			identity();
-		}
+		Mat4();
 
-		static Mat4 multiply(const Mat4& a, const Mat4& b)
-		{
-			Mat4 result;
+		static Mat4 multiply(const Mat4& a, const Mat4& b);
 
-			for (int32 column = 0; column < 4; column++)
-			{
-				for (int32 line = 0; line < 4; line++)
-				{
-					float sum = 0.0f;
-					sum += a.element[line] * b.element[column * 4];
-					sum += a.element[line + 1 * 4] * b.element[column * 4 + 1];
-					sum += a.element[line + 2 * 4] * b.element[column * 4 + 2];
-					sum += a.element[line + 3 * 4] * b.element[column * 4 + 3];
-					result.element[line + column * 4] = sum;
-				}
-			}
-			return result;
-		}
-
-		float operator[](int32 n)
-		{ 
-			LDK_ASSERT(n >= 0 && n < 16, "vector index should be between positive and within vector size");
-			return element[n];
-		}
+		float operator[](int32 n);
 
 		//Note: This does NOT modify the original value. Instead, operates and returns a copy of the matrix.
-		inline Mat4 operator*(const Mat4& other)
-		{
-			return Mat4::multiply(*this, other);
-		}
+		inline Mat4 operator*(const Mat4& other);
 
-		inline Mat4& operator*=(const Mat4& other)
-		{
-			*this = Mat4::multiply(*this, other);
-			return *this;
-		}
+		inline Mat4& operator*=(const Mat4& other);
 
-		Mat4& translate(Vec3& translation)
-		{
-			return translate(translation.x, translation.y, translation.z);
-		}
+		Mat4& translate(Vec3& translation);
 
-		Mat4& translate(float x, float y, float z)
-		{
-			// Create a translation matrix
-			Mat4 translationMatrix;
-			translationMatrix.element[12] = x;
-			translationMatrix.element[13] = y;
-			translationMatrix.element[14] = z;
-			*this *= translationMatrix;
-			return *this;
-		}
+		Mat4& translate(float x, float y, float z);
 
-		Mat4& scale(Vec3& factor)
-		{
-			return scale(factor.x, factor.y, factor.z);
-		}
+		Mat4& scale(Vec3& factor);
 
-		Mat4& scale(float x, float y, float z)
-		{
-			// Create a translation matrix
-			Mat4 scaleMatrix;
-			scaleMatrix.element[0] = x;
-			scaleMatrix.element[5] = y;
-			scaleMatrix.element[10] = z;
-			*this *= scaleMatrix;
-			return *this;
-		}
+		Mat4& scale(float x, float y, float z);
 
-		inline Mat4& rotate(float rad)
-		{
-			Mat4 zRotationMatrix;
-			float costTheta = cosf(rad);
-			float sinTheta = sin(rad);
-			zRotationMatrix.element[0] = costTheta;
-			zRotationMatrix.element[1] = sinTheta;
+		inline Mat4& rotate(float rad);
 
-			zRotationMatrix.element[4] = -sinTheta;
-			zRotationMatrix.element[5] = costTheta;
-			*this *= zRotationMatrix;
-			return *this;
-		}
+		inline void diagonal(float value);
 
-		inline void diagonal(float value)
-		{
-			element[0] = 1.0f;
-			element[5] = 1.0f;
-			element[10] = 1.0f;
-			element[15] = 1.0f;
-		}
+		inline void identity();
 
-		inline void identity()
-		{
-			diagonal(1.0f);
-		}
-
-		void orthographic(float left, float right, float bottom, float top, float near, float far)
-		{
-			float width = right - left;
-			float height = top - bottom;
-			float depth = far - near;
-
-			diagonal(1.0f);
-			//ldk::Mat4 ortho;
-			LDK_ASSERT(width != 0, "Orthographic width can not be zero");
-			LDK_ASSERT(height != 0, "Orthographic height can not be zero");
-			LDK_ASSERT(depth != 0, "Orthographic depth can not be zero");
-			
-			// Diagonal
-			element[0] = 2.0f / width;
-			element[5] = 2.0f / height;
-			element[10] = -(2.0f / depth);
-
-			// Last column
-			element[12] = -((right + left) / width);
-			element[13] = -((top + bottom) / height);
-			element[14] = -((far + near) / depth);
-		}
+		void orthographic(float left, float right, float bottom, float top, float near, float far);
 
 	};
 
-	float lerp(float start, float end, float t) 
-	{
-		return (1 - t) * start + t * end;
-	}
+	LDK_API float lerp(float start, float end, float t); 
 
-	Vec2 lerpVec2(const Vec2& start, const Vec2& end, float t)
-	{
-		Vec2 result;
-		result.x = lerp(start.x, end.x, t);
-		result.y = lerp(start.y, end.y, t);
-		return result;
-	}
+	LDK_API Vec2 lerpVec2(const Vec2& start, const Vec2& end, float t);
 
-	Vec3 lerpVec3(const Vec3& start, const Vec3& end, float t)
-	{
-		Vec3 result;
-		result.x = lerp(start.x, end.x, t);
-		result.y = lerp(start.y, end.y, t);
-		result.z = lerp(start.z, end.z, t);
-		return result;
-	}
+	LDK_API Vec3 lerpVec3(const Vec3& start, const Vec3& end, float t);
 	
-	Vec4 lerpVec4(const Vec4& start, const Vec4& end, float t)
-	{
-		Vec4 result;
-		result.x = lerp(start.x, end.x, t);
-		result.y = lerp(start.y, end.y, t);
-		result.z = lerp(start.z, end.z, t);
-		result.w = lerp(start.w, end.w, t);
-		return result;
-	}
+	LDK_API Vec4 lerpVec4(const Vec4& start, const Vec4& end, float t);
 
 } // ldk
 
