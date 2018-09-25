a66f0c4705ba2cf91c296b424f1e335d402f1199
marciovmf <marciovmf@gmail.com>
2018-09-25 02:23:18 -0300

    Add method for initializing a perspective projection matrix

--- src/include/ldk/ldk_math.h
+++ src/include/ldk/ldk_math.h
@@ -179,6 +179,8 @@ namespace ldk
 
 		void orthographic(float left, float right, float bottom, float top, float near, float far);
 
+		void perspective(float fov, float ratio, float near, float far);
+
 	};
 
 	LDK_API float lerp(float start, float end, float t); 
